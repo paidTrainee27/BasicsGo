03baf7679518d2d394f9ab70bec9e34c2244618d
riyaz <riyaztasleem.mansuri@circles.asia>
2022-05-29 02:44:14 +0530

    new example

--- context/main.go
+++ context/main.go
@@ -1,20 +1,38 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"time"
 )
 
+type msgID string
+
+const (
+	requestId msgID = "msgId"
+)
+
 func main() {
-	http.HandleFunc("/sayhello", hello)
+	hlo := http.HandlerFunc(hello)
+	http.Handle("/sayhello", hellowMiddleware(hlo))
 	fmt.Println("Started listening at 8089")
 	http.ListenAndServe(":8089", nil)
+}
+
+func hellowMiddleware(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		msgId := time.Now().Nanosecond()
+		ctx := context.WithValue(r.Context(), requestId, msgId)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 	fmt.Println("hello request")
+	fmt.Printf("msgId: %d\n", ctx.Value(requestId))
 
 	select {
 	case <-time.After(10 * time.Second):
