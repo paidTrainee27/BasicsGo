25597b69267e140c2bfb5a137a1883067cc7dec3
riyaz <riyaztasleem.mansuri@circles.asia>
2022-05-28 18:47:28 +0530

    basic implementation

--- context/main.go
+++ context/main.go
@@ -1,8 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
+func main() {
+	http.HandleFunc("/sayhello", hello)
+	fmt.Println("Started listening at 8089")
+	http.ListenAndServe(":8089", nil)
+}
 
+func hello(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	fmt.Println("hello request")
 
-func main()  {
-	
+	select {
+	case <-time.After(10 * time.Second):
+		fmt.Fprintf(w, "Hello World")
+	// When you press ctrl + C
+	case <-ctx.Done():
+
+		fmt.Println("Req cancelled")
+		err := ctx.Err()
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
