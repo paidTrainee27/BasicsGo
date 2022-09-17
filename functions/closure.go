10f7dca0f31a387a77c271308107ab050b9fc9ca
riyaz <riyaztasleem.mansuri@circles.asia>
2022-09-18 02:26:40 +0530

    added example

--- functions/closure.go
+++ functions/closure.go
@@ -7,29 +7,30 @@ type cls1 func() int
 var shadowCount int
 
 var isEven func(num int) bool
+
 /*
-A function definition that is not bound to an identifier. 
-Anonymous functions are often arguments being passed to higher-order functions or 
+A function definition that is not bound to an identifier.
+Anonymous functions are often arguments being passed to higher-order functions or
 used for constructing the result of a higher-order function that needs to return a function.
-If the function is only used once, or a limited number of times, an anonymous function may be 
+If the function is only used once, or a limited number of times, an anonymous function may be
 syntactically lighter than using a named function.
 */
-func AnanymousFunc(){
+func AnanymousFunc() {
 	name := "Albert"
 
 	// self Calling
-	func (name string)  {
-		fmt.Println("Welcome"+name)
+	func(name string) {
+		fmt.Println("Welcome" + name)
 	}(name)
 
 	//as a variable for minimal implementation
-	isEven = func(num int) bool{
+	isEven = func(num int) bool {
 		return num%2 == 0
 	}
 
-	intArr := []int{1,2,3,4}
+	intArr := []int{1, 2, 3, 4}
 	for i := 0; i < len(intArr); i++ {
-		if isEven(intArr[i]){
+		if isEven(intArr[i]) {
 			fmt.Println(intArr[i])
 		}
 	}
@@ -50,14 +51,29 @@ func callClosure() {
 func intSeq() cls1 {
 	i := 0
 	return func() int {
-		i++//captures the state of functions variable i
+		i++ //captures the state of functions variable i
 		return i
 	}
 }
 
-
-func ShadowingVar(){
-	//shadowing 
+func ShadowingVar() {
+	//shadowing
 	shadowCount := 2
 	fmt.Println(shadowCount)
+}
+
+func closureVSanonymous() {
+	func() {
+		for i := 0; i < 3; i++ {
+			defer fmt.Println("a:", i) //closures the state of variable i
+		}
+	}()
+	fmt.Println()
+	func() {
+		for i := 0; i < 3; i++ {
+			defer func() {
+				fmt.Println("b:", i) //anonymous func prints i as 3
+			}()
+		}
+	}()
 }
